3569f74d3a385c150d16aec586f44700d80f54a3
AlexChaloner <alex.chaloner@outlook.com>
2023-11-05 15:48:13 +0000

    feat: Create bar charts for every column
    Allows studying of data.
    Also added comparison bar charts so that one can compare how the different data looks when filtering by a column

--- main.py
+++ main.py
@@ -2,49 +2,63 @@ from utils import read_csv
 from data import TitanicData
 import matplotlib.pyplot as plt
 import pandas as pd
+import math
 
 
 def main():
-    csv_object = read_csv("data/train.csv")
+    TRAIN_DATA_FILE = "data/train.csv"
+    csv_object = read_csv(TRAIN_DATA_FILE)
     train_data = TitanicData(csv_object)
 
-    # Let's visualise the training data
-    total = len(train_data.data_as_rows)
-    num_transported = len([row for row in train_data.data_as_rows if row.transported])
+    train_data_df = pd.read_csv(TRAIN_DATA_FILE, index_col="PassengerId")
 
-    print(num_transported, total)
+    train_data_df[['Cabin1', 'Cabin2', 'Cabin3']] = train_data_df['Cabin'].str.split('/', n=3, expand=True)
+    train_data_df["Cabin2"] = train_data_df["Cabin2"].fillna(0).astype(int)
+    # Remove the 'full_name' column
+    train_data_df = train_data_df.drop('Cabin', axis=1)
 
-    print(train_data.data_as_columns)
+    display_all(train_data_df)
+    display_comparisons_discrete(train_data_df, "Transported")
 
 
+def display_all(df: pd.DataFrame):
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    # plt.scatter([row.age for row in train_data], [row.transported for row in train_data])
-    # plt.show()
-
-    # plt.scatter(
-    #     [row.cabin[2] for row in train_data], [row.transported for row in train_data],
-    #     alpha=0.005)
-    # plt.show()
-
-    # plt.scatter(
-    #     [int(row.vip) for row in train_data], [row.transported for row in train_data],
-    #     alpha=0.005)
-    # plt.show()
-
-    # Define colors for transported and Non-transported
-    colors = {False: 'red', True: 'blue'}
-
-    # Create a scatter plot with color-coding based on VIP status
-    plt.scatter([1 if row.vip else 0 for row in train_data],
-                [1 if row.cryo_sleep else 0 for row in train_data],
-                c=[colors[row.transported] for row in train_data],
-                label=[row.transported for row in train_data],
-                alpha=0.1)
+    add_charts_all_columns(df, fig)
     plt.show()
 
 
+def display_comparisons_discrete(df: pd.DataFrame, comparison_column: str):
+    unique_values = df[comparison_column].unique()
+    print(unique_values)
+    if len(unique_values) > 4:
+        print("Warning: More than 4 unique values. Check if you really want to do this?")
+        return
+    for v in unique_values:
+        fig = plt.figure()
+        new_df = df[df[comparison_column] == v].copy(deep=True)
+        new_df = new_df.drop(comparison_column, axis=1)
+        add_charts_all_columns(new_df, fig)
+    plt.show()
 
+
+def add_charts_all_columns(df: pd.DataFrame, fig: plt.Figure):
+    COLUMN_NUMBER = len(df.columns)
+    intSqrtCol = math.ceil(math.sqrt(COLUMN_NUMBER))
+
+    for i, column in enumerate(df.columns):
+        ax = fig.add_subplot(intSqrtCol, intSqrtCol, i+1)
+        ax.set_title(column)
+
+        if df[column].dtype in ['int32', 'int64', 'float64']:
+            ax.hist(df[column], bins=20)
+        elif column == "Name":
+            continue
+        else:
+            counts = df[column].value_counts()
+            counts = counts.sort_index()
+            ax.bar(counts.index, counts)
+    plt.subplots_adjust(wspace=1, hspace=1)
+    
 
 if __name__ == "__main__":
     main()
