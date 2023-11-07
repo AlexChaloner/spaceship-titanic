02d85059b6b3e3ed6852a516c38b5adb8a858f6a
Alex Chaloner <alex.chaloner@outlook.com>
2023-11-07 21:56:32 +0000

    feat: Use exponential randomisers

--- train.py
+++ train.py
@@ -36,27 +36,30 @@ hist_native = make_pipeline(
     ordinal_encoder,
     HistGradientBoostingClassifier(
         random_state=42,
+        max_bins=255,
         categorical_features=categorical_columns,
     ),
 ).set_output(transform="pandas")
 
 # Define a hyperparameter grid for RandomizedSearchCV
 param_dist = {
-    'histgradientboostingclassifier__learning_rate': np.random.uniform(0.01, 1.0, 10),
-    'histgradientboostingclassifier__max_iter': np.random.randint(10, 1000, 10),
-    'histgradientboostingclassifier__max_leaf_nodes': np.random.randint(15, 128, 10),
-    'histgradientboostingclassifier__min_samples_leaf': np.random.randint(1, 9, 10),
-    'histgradientboostingclassifier__l2_regularization': np.random.uniform(0.0, 0.3, 10),
+    'histgradientboostingclassifier__learning_rate': np.random.exponential(0.1, 100),
+    'histgradientboostingclassifier__max_iter': np.random.randint(10, 1000, 100),
+    'histgradientboostingclassifier__max_leaf_nodes': np.random.randint(1, 512, 10),
+    'histgradientboostingclassifier__max_depth': np.random.randint(1, 512, 10),
+    'histgradientboostingclassifier__min_samples_leaf': np.random.randint(1, 50, 10),
+    'histgradientboostingclassifier__l2_regularization': np.random.exponential(0.01, 100),
 }
 
 # Create the RandomizedSearchCV object
 random_search = RandomizedSearchCV(
     hist_native,
     param_distributions=param_dist,
-    n_iter=5,  # Number of random parameter combinations to try
-    cv=5,  # Number of cross-validation folds
+    n_iter=1,  # Number of random parameter combinations to try
+    cv=3,  # Number of cross-validation folds
     scoring='accuracy',
-    random_state=42
+    random_state=42,
+    verbose=3
 )
 
 # Perform random search
@@ -70,5 +73,6 @@ print(best_params)
 # Fit the best model on the entire training dataset
 best_model.fit(X, y)
 y_pred_best = best_model.predict(X)
+print(y_pred_best)
 accuracy_best = accuracy_score(y, y_pred_best)
 print(accuracy_best)
